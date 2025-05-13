ac83633f92ce25a23c8e06294848b929c0b091e3
Sean-mn <b01080080329@gmail.com>
2025-05-13 19:53:28 +0900

    DB 연결

--- Server/SessionServer/SessionServer/Program.cs
+++ Server/SessionServer/SessionServer/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SessionServer.DBContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 if (string.IsNullOrEmpty(connectionString))
     throw new InvalidOperationException("Database connection is null.");
 
-builder.Services.
+builder.Services.AddDbContext<UserDBContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 builder.Services.AddControllers();
 
