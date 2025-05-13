06e27ffea68ef2132ab026c24861cd2c5f641d12
Sean-mn <b01080080329@gmail.com>
2025-05-13 20:51:57 +0900

    UserDBContext 수정

--- Server/SessionServer/SessionServer/DBContexts/UserDBContext.cs
+++ Server/SessionServer/SessionServer/DBContexts/UserDBContext.cs
@@ -7,5 +7,5 @@ public class UserDBContext : DbContext
 {
     public UserDBContext(DbContextOptions<UserDBContext> options) : base(options) { }
     
-    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<User> Users { get; set; }
 }
