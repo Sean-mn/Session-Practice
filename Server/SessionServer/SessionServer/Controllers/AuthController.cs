e0662b35beff6bd85162f63238f102f444e07e5d
Sean-mn <b01080080329@gmail.com>
2025-05-13 19:54:26 +0900

    AuthController 생성

---
+++ Server/SessionServer/SessionServer/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SessionServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
