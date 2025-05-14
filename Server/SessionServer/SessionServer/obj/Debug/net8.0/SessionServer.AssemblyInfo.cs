7b63477b560892fb8cb92cd60c53662bdfb464cc
Sean-mn <b01080080329@gmail.com>
2025-05-14 21:08:23 +0900

    ScoreController 생성

--- Server/SessionServer/SessionServer/obj/Debug/net8.0/SessionServer.AssemblyInfo.cs
+++ Server/SessionServer/SessionServer/obj/Debug/net8.0/SessionServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SessionServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e5ae8efa32295fb2da360a26055c862afc7fd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06af6e3aa7e891764d8a93b5f275e8654806a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("SessionServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SessionServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
